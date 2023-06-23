b2effceff61529c56357f991c3e3978b77b5b371
axelcall123 <axel15calderon@gmail.com>
2023-06-23 00:28:40 -0600

    copy,transfer

--- backend/_general.py
+++ backend/_general.py
@@ -1,5 +1,6 @@
 import os
-
+import shutil
+import json
 def creRenameL(idFolderRaiz: str, nombre: str) -> str:
     # FIXME: para no reahacerlo se puede poner otro parametro par unirlo con navacion carpetas, se puede optimizar
     if os.path.exists(idFolderRaiz+'/'+nombre):  # existe nombre
@@ -11,3 +12,55 @@ def creRenameL(idFolderRaiz: str, nombre: str) -> str:
         return creRenameL(idFolderRaiz, nombre)
     else:  # no existe nombre, retorno el nombre
         return nombre
+
+def copySever(urlTo,urlFrom):
+    for item in os.listdir(urlFrom):
+        url1 = os.path.join(urlTo,item)
+        url2=os.path.join(urlFrom,item)
+        if os.path.exists(url1) and not '.' in item:#existe es carpeta
+            copySever(url1,url2)
+        elif not os.path.exists(url1) and not '.' in item:  # no existe es carpeta
+            shutil.copytree(url2, url1)
+        else:#no existe es archivo
+            rename=creRenameL(urlTo,item)
+            nuevoUrl=os.path.join(urlTo, rename)#renombro por si existe
+            shutil.copy(url2, nuevoUrl)
+
+def transferSersver(urlTo,urlFrom):
+    for item in os.listdir(urlFrom):
+        url1 = os.path.join(urlTo, item)
+        url2 = os.path.join(urlFrom, item)
+        if os.path.exists(url1) and not '.' in item:  # existe es carpeta
+            transferSersver(url1, url2)
+        elif not os.path.exists(url1) and not '.' in item:  # no existe es carpeta
+            shutil.copytree(url2, url1)#copio
+            shutil.rmtree(url2)#elimino tree
+        else:  # no existe es archivo
+            rename = creRenameL(urlTo, item)
+            nuevoUrl = os.path.join(urlTo, rename)  # renombro por si existe
+            shutil.move(url2, nuevoUrl)
+
+def readTxt(url):
+    f = open(url, 'r')
+    file_content=f.read()
+    f.close()
+    return file_content
+
+def listadoJson(url)->json:#posicion es para saber si es el primero, subcarpeta
+    listado = os.listdir(url)
+    txtJson=''
+    for iI in range(len(listado)):
+        if '.' in listado[iI]:# es archivo
+            if iI<len(listado)-1:# ultimo item
+                txtJson += '"'+listado[iI]+'":"'+readTxt(os.path.join(url,listado[iI]))+'",'
+            else:
+                txtJson += '"'+listado[iI]+'":"'+readTxt(os.path.join(url,listado[iI]))+'"'
+                return txtJson
+        else:# es carpeta
+            if iI < len(listado)-1:  # ultimo item
+                txtJson += '"'+listado[iI]+'":{'+listadoJson(os.path.join(url,listado[iI]))+'},'
+            else:
+                txtJson += '"'+listado[iI]+'":{'+listadoJson(os.path.join(url,listado[iI]))+'}'
+                return txtJson
+    return ''
+
--- backend/server.py
+++ backend/server.py
@@ -1,25 +1,17 @@
 from flask import Flask, jsonify, request, Response
-# import boto3
 # from ..Test.key import *
 from varDef import *
 import _general as _G
 import os
 import shutil
+import json
 app = Flask(__name__)
-
-# s3 = boto3.client(
-#     's3',
-#     aws_access_key_id=acces_key,
-#     aws_secret_access_key=secret_acces_key
-# )
-
 
 @app.route('/')
 def index():
     return 'Welcome to Flask'
 
-
-@app.route('/create', methods=['GET','POST'])
+@app.route('/create', methods=['POST'])#crea archviso
 def archivoC():
     '''{
     "ruta":"/ar/",
@@ -35,21 +27,61 @@ def archivoC():
     f.close()  # siempre cerrar
     return jsonify({'mensaje': 'archivo creado',})
 
-
-@app.route('/create', methods=['GET', 'DELETE'])
+@app.route('/create', methods=['DELETE'])#elimina archivo
 def archivoD():
+    '''{
+    "ruta":"/ar/",
+    "nombre":"a(1).txt" {opcional}
+    }'''
     rs = request.get_json()  # json
     ruta=rutaEspecifica+rs["ruta"]
     if "nombre" in rs:
         ruta=ruta+rs["nombre"]
-    if (os.path.exists(ruta)):  # existe ruta
-        print('E')
-        shutil.rmtree(ruta)
-        return jsonify({'mensaje': 'archivo eliminado', })
-    else:
-        print('N E')
-        return jsonify({'mensaje': 'ruta no encontrada', })
+    return jsonify({'mensaje': _G.deleteSever(ruta)})
     
+@app.route('/copy', methods=['POST'])  # copiar archivo server->server; bucket->server
+def copySB():
+    '''{
+    "from":"/A/a.txt",
+    "to":"/B/"
+    }'''
+    rs = request.get_json() # json
+    if 'from' in rs and 'to' in rs:# de server a server
+        _G.copySever(rutaEspecifica+rs["to"],rutaEspecifica+rs["from"])
+        return jsonify({'mensaje': 'archivos copiado'})
+    elif 'from' in rs:# de bucket a server
+        print()
+        return jsonify({'mensaje': 'copio json'})#FIXME:
+
+@app.route('/copy', methods=['GET'])# listado de los archivos del server
+def copyLS():
+    rs = request.get_json()  # json
+    if 'to' in rs:  # de server a bucket
+        txt="{"+_G.listadoJson(rutaEspecifica+rs["to"])+"}"
+        return jsonify({'mensaje': json.loads(txt)})
+
+@app.route('/transfer', methods=['POST'])# tranfiere archivo server->server; bucket->server
+def tranferSB():
+    '''{
+    "from":"/A/a.txt",
+    "to":"/B/"
+    }'''
+    rs = request.get_json()  # json
+    if 'from' in rs and 'to' in rs:  # de server a server
+        _G.transferSersver(rutaEspecifica+rs["to"], rutaEspecifica+rs["from"])
+        return jsonify({'mensaje': 'archivos transferidos'})
+    elif 'from' in rs:  # de bucket a server
+        print()
+        return jsonify({'mensaje': 'tranfiero json'})# FIXME:
+
+@app.route('/transfer', methods=['GET'])  # listado de los archivos del server
+def transferLS():
+    rs = request.get_json()  # json
+    if 'to' in rs:  # de server a bucket
+        txt = "{"+_G.listadoJson(rutaEspecifica+rs["to"])+"}"
+        for file in os.listdir(rutaEspecifica+rs["to"]):
+            _G.deleteSever(os.path.join(rutaEspecifica+rs["to"],file))
+        return jsonify({'mensaje': json.loads(txt)})
 
 if __name__ == '__main__':
     # debug modo solo sirve para que se acutalice automaticamente
